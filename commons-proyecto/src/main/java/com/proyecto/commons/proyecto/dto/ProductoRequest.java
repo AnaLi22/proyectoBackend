34fbb42dee365e700ddb44c1531cd1dc8ecb01d5
HkingMYN <ing.saulmynpastor@gmail.com>
2025-06-18 15:12:24 -0600

    UPDATE VALIDATIONS

--- commons-proyecto/src/main/java/com/proyecto/commons/proyecto/dto/ProductoRequest.java
+++ commons-proyecto/src/main/java/com/proyecto/commons/proyecto/dto/ProductoRequest.java
@@ -1,19 +1,21 @@
 package com.proyecto.commons.proyecto.dto;
 
-public record ProductoRequest(
-		
-		String nombre, 
-		
-		
-		String descripcion, 
-		
-		
-		Double precio, 
-		
-		
-		Integer stock
-		
-		
-		){
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-}
+public record ProductoRequest(
+    @NotBlank(message = "El nombre no puede ser nulo o vacío")
+    String nombre, 
+
+    @NotBlank(message = "La descripción no puede ser nula o vacía")
+    String descripcion, 
+
+    @NotNull(message = "El precio no puede ser nulo")
+    @Min(value = 0, message = "El precio no puede ser negativo")
+    Double precio, 
+
+    @NotNull(message = "El stock no puede ser nulo")
+    @Min(value = 0, message = "El stock no puede ser negativo")
+    Integer stock
+) {}
